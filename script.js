1ac5d19bc3be5fa5fc74abfb27af879a22521e31
John L Spetz <JohnLSpetz@zoho.com>
2020-06-07 18:29:38 -0400

    Add max length of 128 and require one digit

--- script.js
+++ script.js
@@ -14,24 +14,28 @@ function generatePassword() {
     upper:
       ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    digit:
+      ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     punct:
       ['~', '!', '#', '$', '%', '^', '*', '-', '+', '.', ',', ':', ';'],
     initialized: false,
     passWord: "",
     hasUpper: false,
+    hasDigit: false,
     hasPunct: false,
-    BuildPassword: function(startPass, needUpper, needPunct) {
+    BuildPassword: function(startPass, needUpper, needDigit, needPunct) {
       this.initialized = true;
       this.passWord = startPass;
       //Note reversal of boolean parameters, so pass in whether
       //this character type is required not if it is present!
       this.hasUpper = !needUpper;
+      this.hasDigit = !needDigit;
       this.hasPunct = !needPunct;
     },
     getRand: function(rangeFromZero) {
       return Math.floor(Math.random() * rangeFromZero);
     },
-    getPassword: function(minLen) {
+    getPassword: function(minLen, maxLen) {
       var complexEnough = false;
       //For future reuseability initialize internal fields unless this step was
       //already performed.
@@ -39,9 +43,18 @@ function generatePassword() {
         this.BuildPassword("", true, true);
       }
       this.initialized = false;
-      while ( (this.passWord.length < minLen) || !complexEnough ) {
+      //Unless maximum password length has been reached continue
+      //adding random characters until both the minimum length
+      //and complex enough requrements have been satisfied.
+      //Note that lower case letters are more likely to be added
+      //than upper case letters or punctuation, but an extremely
+      //long password is very unlikely.
+      while ( (this.passWord.length < minLen) ||
+              !complexEnough &&
+              (this.passWord.length < maxLen) ) {
         //console.log("passWord.length="+this.passWord.length);
-        //DEBUG by uncommenting: if (confirm("Stop early?")) { break; }
+        //DEBUG by uncommenting:
+        //if (confirm("Password="+this.passWord+"\nStop early?")) { break; }
         //First random number decides from which group to get the next
         //character added to the password.  Prioritize lower case but
         //require an upper case letter and a punctuation character.
@@ -52,12 +65,17 @@ function generatePassword() {
         case 0:
           this.passWord += this.upper[this.getRand(this.upper.length)];
           this.hasUpper = true;
-          complexEnough = this.hasUpper && this.hasPunct; 
+          complexEnough = this.hasUpper && this.hasDigit && this.hasPunct; 
           break;
         case 1:
+          this.passWord += this.digit[this.getRand(this.digit.length)];
+          this.hasDigit = true;
+          complexEnough = this.hasUpper && this.hasDigit && this.hasPunct; 
+          break;
+        case 2:
           this.passWord += this.punct[this.getRand(this.punct.length)];
           this.hasPunct = true;
-          complexEnough = this.hasUpper && this.hasPunct; 
+          complexEnough = this.hasUpper && this.hasDigit && this.hasPunct; 
           break;
         default:
           this.passWord += this.lower[this.getRand(this.lower.length)];
@@ -67,7 +85,7 @@ function generatePassword() {
       }
     }
   }
-  buildPassword.getPassword(8);
+  buildPassword.getPassword(8,128);
   return buildPassword.passWord;
 }
 
